f2c58dba73ebd3f1c2950b32096dc88fa1783395
Damien Dassieu <dassieu.damien@gmail.com>
2026-04-18 15:36:44 +0200

    fix(makefile): rework makefile
    Signed-off-by: Damien Dassieu <dassieu.damien@gmail.com>

--- hack/webhooks/cleanup-injector.sh
+++ hack/webhooks/cleanup-injector.sh
@@ -7,6 +7,10 @@ fi
 
 CERT_DIR=$1
 
+sed -i '/caBundle:/d' config/local/run/webhook/webhook.yaml || true
+sed -i '/caBundle:/d' config/local/run/crd/patches/webhook_in_remotesyncers.yaml || true
+sed -i 's/\(tls\.crt: \).*/\1/; s/\(tls\.key: \).*/\1/' config/local/deploy/webhook/secret.yaml || true
+
 rm -rf config/local/run/webhook/webhook.yaml
 rm -rf config/local/run/crd
 rm -rf config/local/deploy/webhook/webhook.yaml
